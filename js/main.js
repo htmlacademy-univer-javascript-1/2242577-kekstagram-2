8c6c8c22d76d9c18599075fadc150d3bf23ee5a5
Polina <Polina-shipanova2812@ya.ru>
2023-04-04 15:18:31 +0500

    object generation task completed

--- js/main.js
+++ js/main.js
@@ -1,4 +1,22 @@
-// eslint-disable-next-line no-unused-vars
+const PFHOTOS_COUNT = 25;
+const NAMES = ['Александр', 'Олежа', 'Дмитрий', 'Паша', 'Арсений'];
+const DESCRIPTION = Array.from({length: PFHOTOS_COUNT},(_, i) =>`Описание ${i}`);
+const COMMENT =['Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'];
+const COMMENT_IDS= [];
+
+function checkStringLength (string, length) {
+  return string.length <= length;
+}
+
+const getRandomLikes = () => getRandoInt(15,200);
+const getRandomElement = (arr) => arr[getRandoInt(0, arr.length-1)];
+
+
 function getRandoInt(min, max){
   if(min < 0 || max < 0){
     throw new RangeError('Числа должны быть положительными');
@@ -12,10 +30,41 @@ function getRandoInt(min, max){
   return Math.round(Math.random()*(max-min)+min);
 }
 
-// eslint-disable-next-line no-unused-vars
-const isCorrectLength = (line, maxLenght) => {
-  if (typeof line !== 'string'){
-    throw new RangeError('Значение line должно быть строкой');
+const getId = (()=>{
+  let id =1;
+  return() => id++;
+})();
+
+function getCommentId(){
+  let id = getRandoInt(1,1000);
+  while (COMMENT_IDS.includes(id)){
+    id = getRandoInt(1, 1000);
   }
-  return line.length <= maxLenght;
-};
+  return id;
+}
+function generateComment(){
+  const commentText =[];
+  for (let i=0; i< getRandoInt(1,2); i++){
+    commentText.push(getRandomElement(COMMENT));}
+  return {
+    id: getCommentId(),
+    avatar: `img/avatar-${getRandoInt(1,6)}.svg`,
+    massage: commentText.join(''),
+    name: getRandomElement(NAMES)};
+}
+
+function generateDescription(){
+  const comments = Array.from({length: getRandoInt(0,3)},generateComment);
+  const id = getId();
+  return{
+    id: id,
+    url: `photos/${id}.jpg`,
+    description: DESCRIPTION[id-1],
+    likes: getRandomLikes(),
+    comments: comments
+  };
+}
+
+const descriptions = Array.from({length: PFHOTOS_COUNT}, generateDescription);
+
+checkStringLength(descriptions, PFHOTOS_COUNT);
